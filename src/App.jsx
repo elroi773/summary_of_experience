2db5dd776d44217c300c51968e53539b46b22ea6
elroi773 <s2457@e-mirim.hs.kr>
2025-10-22 11:46:30 +0900

    경험 추가 페이지 프론트

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-import CheckoutPage from "./CheckoutPage";
+import CheckoutPage from "./Checkoutpage";
 import MyStrength from "./MyStrength";
+import AddExperience from "./AddExperience";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<CheckoutPage />} /> // ✅ 첫 시작 화면
       <Route path="/my-strength" element={<MyStrength />} />
+      <Route path="/addexperience" element={<AddExperience />} />
     </Routes>
   );
 }
