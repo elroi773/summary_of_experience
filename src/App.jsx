386277ba537fd06a712ae492643aa9a4de8a89be
elroi773 <s2457@e-mirim.hs.kr>
2025-10-26 21:46:57 +0900

    db 연결

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import CheckoutPage from "./Checkoutpage";
 import MyStrength from "./MyStrength";
 import AddExperience from "./AddExperience";
+import Result from "./Result";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path="/" element={<CheckoutPage />} /> // ✅ 첫 시작 화면
       <Route path="/my-strength" element={<MyStrength />} />
       <Route path="/addexperience" element={<AddExperience />} />
+      <Route path="/result" element={<Result />} />
     </Routes>
   );
 }
